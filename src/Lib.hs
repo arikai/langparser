02b6121cae007ac4cfb19338d19b8b492b750d70
Yaroslav Rogov <rogovyaroslav@gmail.com>
2020-11-09 12:13:22 +0300

    feat: Working parser

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,8 +4,23 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Lib
-    ( someFunc
+    (
+      interpretArgs,
+      parseProgram,
+      Program (..),
+      Constant (..),
+      Identifier (..),
+      Operand (..),
+      Statement (..),
+      Expression (..),
+      UnaryOp (..),
+      BinaryOp (..),
+
+      Warning,
+      WarningReason
     ) where
+
+import System.Environment (getArgs)
 
 -- I import qualified so that it's clear which
 -- functions are from the parsec library:
@@ -18,13 +33,19 @@ import Control.Applicative hiding (many, (<|>))
 
 -- Get the Identity monad from here:
 import Control.Monad.Identity (Identity)
-import Text.RawString.QQ
+import Data.String.Here.Interpolated (i, iTrim)
+import Data.String.Here.Uninterpolated (here, hereLit)
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+import qualified Data.List as List
+import qualified Data.Maybe as Maybe
 
 import Text.Parsec
 import Text.Printf (printf)
--- import Text.Parsec (char, string, oneOf, choice, many, optional, chainl1, (<|>), (<?>))
 
--- import Data.Either
+import Data.Typeable (typeOf)
+
 import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.Error as Parsec.Error
 
@@ -35,7 +56,7 @@ import Data.Bool (bool)
 data Program = Program [Statement] deriving (Show, Eq)
 
 data Constant = Constant Integer deriving (Show, Eq)
-data Identifier = Identifier String deriving (Show, Eq)
+data Identifier = Identifier String deriving (Show, Eq, Ord)
 data Operand  =
   OpConstant Constant
   | OpIdentifier Identifier
@@ -85,6 +106,22 @@ binop Equals = \a -> \b -> (boolToInt (a == b))
 -- <Описание вычислений> ::= <Список операторов>
 -- <Программа> ::= <Объявление переменных> <Описание вычислений> .
 
+data Warning = Warning WarningReason SourcePos deriving Show
+
+data WarningReason =
+  Redeclared Identifier
+  | Undeclared Identifier
+  deriving Show
+
+data Context = Context (Set Identifier) ([Warning]) deriving Show
+newContext = Context (Set.empty) ([])
+isDeclared :: Identifier -> Context -> Bool
+isDeclared i (Context s _) = Set.member i s
+declare :: Identifier -> Context -> Context
+declare i (Context s ws) = Context (Set.insert i s) ws
+addWarning :: WarningReason -> SourcePos -> Context -> Context
+addWarning reason pos (Context s ws) = Context s ((Warning reason pos) : ws)
+warnings (Context _ ws) = ws
 
 cons u s = u : s
 wrap s = [s]
@@ -92,7 +129,7 @@ void s = ()
 void2 _ _ = ()
 void3 _ _ _ = ()
 
-spaces1 :: Parsec String () ()
+spaces1 :: Parsec String Context ()
 spaces1 = void <$> many1 space
 
 newlineLessSpace :: (Stream s m Char) => ParsecT s u m Char
@@ -101,19 +138,19 @@ newlineLessSpace = satisfy (\c -> (isSpace c) && (c /= '\n'))
 
 lineterm = (oneOf ";\n")
 
-eol :: Parsec String () ()
+eol :: Parsec String Context ()
 eol = void <$> many1 (void3 <$> (skipMany newlineLessSpace)
                       <*> lineterm
                       <*> (skipMany newlineLessSpace))
 
 -- <Const> ::= <Цифра> <Const> | <Цифра>
-constant :: Parsec String () Constant
+constant :: Parsec String Context Constant
 constant = (Constant . read) <$> (many1 . oneOf $ ['0' .. '9'])
   <?> "numerical constant"
 
 -- <Идент> ::= <Буква> <Идент> | <Буква>
-ident :: Parsec String () Identifier
-ident = Identifier <$> (many1 . oneOf $ ['a'..'z'])
+identifier :: Parsec String Context Identifier
+identifier = Identifier <$> (many1 . oneOf $ ['a'..'z'])
   <?> "identifier of alphabetic characters "
 
 -- <Ун.оп.> ::= "-"|"not"
@@ -125,7 +162,7 @@ unaryOp = negation <|> logicalNot
   <?> "unary operation (- or not)"
 
 -- <Бин.оп.> ::= "-" | "+" | "*" | "/" |"<"|">"|"=="
-binaryOp :: Parsec String () BinaryOp
+binaryOp :: Parsec String Context BinaryOp
 binaryOp =
   (\case
         "-" -> Minus
@@ -140,42 +177,60 @@ binaryOp =
 
 -- <Операнд> ::= <Идент> | <Const>
 operand =
-  (OpConstant <$> constant) <|> (OpIdentifier <$> ident)
+  (Operand) <$> ((OpConstant <$> constant) <|> (OpIdentifier <$> identifier))
 
-unaryAp = (\o -> \_ -> \a -> UnaryAp o a) <$> unaryOp <*> spaces <*> subexpr
 -- -- <Выражение> ::= <Ун.оп.> <Подвыражение> | <Подвыражение>
-expr :: Parsec String () Expression
-expr = (unaryAp <|> subexpr) <?> "expression with optional unary op"
+-- -- <Подвыражение> :: = ( <Выражение> ) | <Операнд> | < Подвыражение > <Бин.оп.> <Подвыражение>
+
+expr :: Parsec String Context Expression
+expr = binaryAp <|> operand
 
 binaryAp =
-  (\s1 -> \_ -> \b -> \_ -> \s2 -> BinaryAp b s1 s2) <$>
-  subexpr <*> spaces <*> binaryOp <*> spaces <*> subexpr
-  <?> "binary operation application in form of "++
-       "<expression> <op> <expression"
+  ( chainl1
+      subexpr
+      ( (\b -> \s1 -> \s2 -> BinaryAp b s1 s2)
+          <$> try (do spaces; b <- binaryOp; spaces; return b)
+      )
+  )
+    <?> "binary operation application in form of "
+      ++ "<expression> <op> <expression"
 
--- <Подвыражение> :: = ( <Выражение> ) | <Операнд> | < Подвыражение > <Бин.оп.> <Подвыражение>
-subexpr :: Parsec String () Expression
-subexpr =
-  (((Operand) <$> operand)
-  <|> expr
-  <|> binaryAp)
-  <?> "one of: operand (const or id), expression, binary op application"
+subexpr :: Parsec String Context Expression
+subexpr = (unaryAp <|> operand) <?> "operand with optional unary op"
+
+unaryAp = (\o -> \_ -> \a -> UnaryAp o a) <$> unaryOp <*> spaces <*> subexpr
+
+
+declareVar i pos c =
+  case isDeclared i c of
+    True -> addWarning (Redeclared i) pos c
+    False -> declare i c
+
+declaredIdentifier =
+  do
+    pos <- getPosition
+    id <- identifier
+    modifyState (declareVar id pos)
+    return id
 
 -- <Список переменных> ::= <Идент> | <Идент> , <Список переменных>
 -- <Объявление переменных> ::= Var <Список переменных>
+declaration :: Parsec String Context Statement
 declaration = (do
-  _ <- string "Var"
-  _ <- spaces1
-  ids <- sepBy1 ident spaces1
+  string "Var"; spaces1
+  ids <- sepBy1 declaredIdentifier spaces1
   return (Declaration ids)) <?> "variable declaration: Var a b c..."
+
 
 -- <Присваивание> ::= <Идент> = <Выражение>
 assignment = (do
-  var <- ident
-  _ <- spaces
-  _ <- char '='
-  _ <- spaces
+  pos <- getPosition
+  var <- identifier
+  spaces; char '='; spaces
   ex <- expr
+  modifyState (\c -> case isDeclared var c of
+                       True -> c
+                       False -> addWarning (Undeclared var) pos c)
   return (Assignment var ex)) <?> "assignment: a = <expr...>"
 
 -- -- <Оператор>::=<Присваивание> |<Сложный оператор>
@@ -184,29 +239,30 @@ assignment = (do
 operator = (assignment <|> complexOperator)
 complexOperator = cycleOperator
 cycleOperator = (do
-  _ <- string "WHILE"
-  _ <- spaces1
-  ex <- expr
-  _ <- spaces1
-  _ <- string "DO"
-  _ <- spaces1
+  string "WHILE"; spaces1;
+  ex <- try binaryAp
+  spaces1; string "DO"; spaces1
   op <- operator
   return (While ex op))
   <?> "a while loop: WHILE <expr> DO <assignment or while loop>"
 
 line content = (\_ -> \c -> \_ -> c) <$> spaces <*> content <*> eol
 
+data ParseResult = ParseResult Program Context deriving (Show)
 -- <Список операторов> ::= <Оператор> | <Оператор> <Список операторов>
 -- <Описание вычислений> ::= <Список операторов>
 -- <Программа> ::= <Объявление переменных> <Описание вычислений> .
+program :: Parsec String Context ParseResult
 program =
   do
     dec <- many . try . line $ declaration
     calc <- many . try . line $ operator
-    _ <- (void2 <$> spaces <*> eof) <?> "end of input"
-    return (Program (dec ++ calc))
+    (spaces >> eof) <?> "end of input"
+    state <- getState
+    return (ParseResult (Program (dec ++ calc)) state)
 
 red = "\x001b[31m"
+yellow = "\x001b[33m"
 bold = "\x001b[1m"
 underline = "\x001b[4m"
 reset = "\x001b[0m"
@@ -217,10 +273,12 @@ underlineSubstring t col length resetTo =
   in left ++ underline ++ red ++ target ++ reset ++ resetTo ++ right
 
 indentLine indent line = printf "% *s %s" (indent + 1) "|" line
-highlightContext :: String -> Int -> Int -> Int -> String
-highlightContext text line column lookaround =
-  let context = take lookaround . drop (line-(div lookaround 2) - 1) . lines $ text
-      (left, target : right) = splitAt (div lookaround 2) context
+highlightContext :: String -> SourcePos -> Int -> String
+highlightContext text pos lookaround =
+  let line = sourceLine pos
+      column = sourceColumn pos
+      context = take (2 * lookaround + 1) . drop (line - lookaround - 1) . lines $ text
+      (left, (target : right)) = splitAt (lookaround) context
       indent = (length . show $ line) + 2
   in bold
      ++ unlines(
@@ -231,49 +289,136 @@ highlightContext text line column lookaround =
 
 fmtUnexpected :: [Parsec.Error.Message] -> String
 fmtUnexpected msgs =
-  case [s | Parsec.Error.UnExpect s <- msgs] of (msg : []) -> msg
+  case [s | Parsec.Error.UnExpect s <- msgs]
+       ++ [s | Parsec.Error.SysUnExpect s <- msgs]
+  of
+    [] -> "???"
+    (msg : _) -> msg
 
 fmtExpected :: [Parsec.Error.Message] -> String
 fmtExpected msgs =
   unlines ["  - " ++ s | Parsec.Error.Expect s <- msgs]
 
-pparse rules source text =
-  putStrLn $ case (Parsec.parse rules source text) of
-               Right(result) -> show result
+fmtWarnings :: [Warning] -> String
+fmtWarnings warns =
+  unlines
+  . (map (\s -> " - " ++ s))
+  . (map (\(Warning reason pos) ->
+            case reason of
+             Undeclared (Identifier id) ->
+               [i|Undeclared variable used: ${id}|]
+             Redeclared (Identifier id) ->
+               [i|Redeclared variable: ${id}|]
+                 )) $ warns
+
+fmtWarningReason reason =
+  case reason of
+    Undeclared id ->
+      [i|Undeclared variable used: ${id}|]
+    Redeclared id ->
+      [i|Redeclared variable: ${id}|]
+
+data ReportSetting =
+  SourceHighlight String
+  | MessageHighlight String
+  | SourceContextLookaround Int
+
+defaultReportSettings =
+  [SourceHighlight bold,
+   MessageHighlight (bold ++ red),
+   SourceContextLookaround 2]
+
+findSetting _ [] = error "impossible"
+findSetting f (x : xs) =
+  case f x of
+    Just(x) -> x
+    Nothing -> findSetting f xs
+
+sourceHighlightSetting =
+  findSetting (\case (SourceHighlight v) -> Just(v); _ -> Nothing)
+
+messageHighlightSetting =
+  findSetting (\case (MessageHighlight v) -> Just(v); _ -> Nothing)
+
+sourceContextLookaroundSetting =
+  findSetting (\case (SourceContextLookaround v) -> Just(v); _ -> Nothing)
+
+
+findFirst f l =
+  (Maybe.fromJust) . (List.find f) $ l
+
+report text pos message description userSettings =
+     [iTrim|
+${sourceHighlight}${source}:${line}:${column}${reset} ${messageHighlight}${message}${reset}
+${highlightContext text pos contextLookaround}
+
+${description}
+|]
+       where
+            settings = (userSettings ++ defaultReportSettings)
+            sourceHighlight = sourceHighlightSetting settings
+            messageHighlight = messageHighlightSetting settings
+            contextLookaround = sourceContextLookaroundSetting settings
+            source = sourceName pos
+            line = sourceLine pos
+            column = sourceColumn pos
+
+reportWarnings text warns =
+  unlines
+  . (map (\(Warning reason pos) -> report text pos (fmtWarningReason reason) "" sets))
+  $ warns
+  where sets = [
+          SourceHighlight "",
+          MessageHighlight yellow,
+          SourceContextLookaround 1]
+
+parseProgram :: String -> String -> Either String Program
+parseProgram source text =
+  case (Parsec.runParser program newContext source text) of
+               Right(ParseResult prog (Context _ ws)) ->
+                 case ws of
+                   [] -> Right(prog)
+                   warnings ->
+                     Left . (reportWarnings text) $ warnings
                Left(error) ->
                  let pos = errorPos error
-                     line = sourceLine pos
-                     column = sourceColumn pos
                      msgs = Parsec.Error.errorMessages error
-                 in
-                   unlines
-                   [
-                     (printf "%s%s:%d:%d: parse error%s"
-                      bold
-                      source line column
-                      reset),
-                     highlightContext text line column 3,
-                     "",
-                     (printf "%sUnexpected input:%s %s"
-                      bold reset (fmtUnexpected msgs)),
-                     (printf "%sExpected on of:%s\n%s"
-                      bold reset (fmtExpected msgs))
-                   ]
+                 in Left(
+                   report text pos "parse error"
+                     [iTrim|
+${bold}Unexpected input:${reset} ${fmtUnexpected msgs}
+${bold}Expected on of:${reset}
+${fmtExpected msgs}
+|] [])
 
-someFunc :: IO ()
-someFunc =
-  pparse program "(source)" [r|
+parseText source text =
+   putStrLn
+  . (\case
+        Right(result) -> show result
+        Left(error) -> error)
+  $ (do
+        prog <- parseProgram source text
+        Right(prog)
+    )
 
+-- -- For debugging purposes
+-- pparse rules text =
+--   putStrLn $
+--     case (Parsec.runParser rules newContext "(source)" text) of
+--       Right (result) ->
+--         show result
+--       Left (error) ->
+--         show error
+-- interpretArgs :: IO ()
+-- interpretArgs =
+--   pparse program [hereLit|
+-- Var a;
+-- a = ------10;
+-- |]
 
-                   Var a b;
-                   Var c d;;;
-
-
-                   Var e;
-                   Var f;
-                   WHILE 1 DO a = 3;
-                   WHILE 1 DO a = 4;
-                   WHILE 1 DO a = 5
-                   WHILE 1 DO a = 6
-                   WHILE 1 DO a = 7
-                   |]
+interpretArgs :: IO ()
+interpretArgs  =
+  do
+    files <- getArgs
+    mapM (\fn -> do (readFile fn) >>= (parseText fn)) files
+    return ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,159 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Test.Hspec
+-- import Test.QuickCheck
+import Lib
+
+import Data.String.Here.Uninterpolated (here, hereLit)
+
+
+parse text = parseProgram "(test)" text
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    describe "Lib.parseProgram" $ do
+      it "parses var declaration" $ do
+        parse "Var a b c;" `shouldBe` Right (
+                  Program [Declaration [Identifier "a",
+                                         Identifier "b",
+                                         Identifier "c"]])
+
+      it "parses multiple var declarations" $ do
+        parse "Var a; Var b; Var c;" `shouldBe` Right(
+          Program [
+              Declaration [Identifier "a"],
+              Declaration [Identifier "b"],
+              Declaration [Identifier "c"]])
+      it "parses simple assignments" $ do
+        parse "Var a; a = 10;" `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+                Assignment (Identifier "a") (Operand (OpConstant (Constant 10)))
+              ])
+      it "parses sequential unary operations" $ do
+        parse "Var a; a = ----10;" `shouldBe` Right (
+          Program [
+            Declaration [Identifier "a"],
+            Assignment
+              (Identifier "a")
+              (UnaryAp Negation
+               (UnaryAp Negation
+                (UnaryAp Negation
+                 (UnaryAp Negation (Operand (OpConstant (Constant 10)))))))
+                  ])
+      it "parses all unary operations" $ do
+        parse "Var a; a = not -10;" `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+              Assignment
+                (Identifier "a")
+                (UnaryAp LogicalNot
+                  (UnaryAp Negation (Operand (OpConstant (Constant 10)))))
+              ])
+      it "parses simple binary operation" $ do
+        parse "Var a; a = 2 + 3;" `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+              Assignment
+                (Identifier "a")
+                (BinaryAp Plus
+                  (Operand (OpConstant (Constant 2)))
+                  (Operand (OpConstant (Constant 3))))
+              ])
+      it "parses nested binary operation left-associatively" $ do
+        parse "Var a; a = 1 + 2 + 3 + 4 + 5;" `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+              Assignment
+                (Identifier "a")
+                (BinaryAp Plus
+                 (BinaryAp Plus
+                  (BinaryAp Plus
+                   (BinaryAp Plus
+                    (Operand (OpConstant (Constant 1)))
+                    (Operand (OpConstant (Constant 2))))
+                   (Operand (OpConstant (Constant 3))))
+                  (Operand (OpConstant (Constant 4))))
+                 (Operand (OpConstant (Constant 5))))
+              ])
+      it "parses all binary operations" $ do
+        let op1 = (Operand (OpConstant (Constant 1)))
+            op2 = (Operand (OpConstant (Constant 2)))
+          in
+          parse [hereLit|
+                        Var a;
+                        a = 1 - 2;
+                        a = 1 + 2;
+                        a = 1 * 2;
+                        a = 1 / 2;
+                        a = 1 < 2;
+                        a = 1 > 2;
+                        a = 1 == 2;
+                        |]
+          `shouldBe` Right (
+              Program [
+                  Declaration [Identifier "a"],
+                  Assignment (Identifier "a") (BinaryAp Minus op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Plus op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Mult op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Div op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Lesser op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Greater op1 op2),
+                  Assignment (Identifier "a") (BinaryAp Equals op1 op2)
+                  ])
+      it "parses while expressions" $ do
+        parse "Var a; a = 0; WHILE a < 42 DO a = a+1;" `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+              Assignment
+                (Identifier "a")
+                (Operand (OpConstant (Constant 0))),
+              While (BinaryAp Lesser
+                      (Operand (OpIdentifier (Identifier "a")))
+                      (Operand (OpConstant (Constant 42))))
+                (Assignment (Identifier "a")
+                  (BinaryAp Plus
+                    (Operand (OpIdentifier (Identifier "a")))
+                    (Operand (OpConstant (Constant 1)))))
+              ])
+      it "parses nested while expressions" $ do
+        parse "Var a; WHILE 1 DO WHILE 2 DO WHILE 3 DO a = 42;"
+          `shouldBe` Right (
+          Program [
+              Declaration [Identifier "a"],
+              While (Operand (OpConstant (Constant 1)))
+                (While (Operand (OpConstant (Constant 2)))
+                  (While (Operand (OpConstant (Constant 3)))
+                    (Assignment
+                     (Identifier "a")
+                     (Operand (OpConstant (Constant 42))))))
+                  ])
+      it "parses simple program" $ do
+        parse [hereLit|
+Var i inc limit;;;;;
+Var cnt;
+
+i = 0
+inc = 10
+limit = 10
+cnt = 0
+
+WHILE i < limit DO cnt = cnt + inc
+|]
+          `shouldBe` Right (
+              Program [
+                  Declaration [Identifier "i", Identifier "inc", Identifier "limit"],
+                  Declaration [Identifier "cnt"],
+                  Assignment (Identifier "i") (Operand (OpConstant (Constant 0))),
+                  Assignment (Identifier "inc") (Operand (OpConstant (Constant 10))),
+                  Assignment (Identifier "limit") (Operand (OpConstant (Constant 10))),
+                  Assignment (Identifier "cnt") (Operand (OpConstant (Constant 0))),
+                  While (BinaryAp Lesser
+                          (Operand (OpIdentifier (Identifier "i")))
+                          (Operand (OpIdentifier (Identifier "limit"))))
+                  (Assignment (Identifier "cnt")
+                    (BinaryAp Plus
+                      (Operand (OpIdentifier (Identifier "cnt")))
+                      (Operand (OpIdentifier (Identifier "inc")))))
+                  ])
